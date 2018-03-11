a3f7166bd1e4f9fd0a3c078d13506d119294c897
Vladyslav Lubeznyy <avinean@gmail.com>
2018-03-11 14:34:31 +0200

    changed router uri

--- public_html/app.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html><html lang="uk-UA"><head><meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <title>app-php</title>
-    <link href="../css/main.css" rel="stylesheet">
-</head>
-<body>
-    <noscript>Для перегляду сторінки ввімкніть підтримку JavaScript у вашому браузері</noscript>
-    <div id="root"></div>
-    <script type="text/javascript" src="../js/main.js"></script></body></html>
---
+++ public_html/text.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+<a href="#page1" class="logo-title">
+    <div class="logo">logo</div>
+    <div class="title">Title</div>
+</a>
+<div class="faq"><a href="#page4"> Про {Title} </a></div>
+</header>
+    
+<div class="form">
+    <input type="email" placeholder="Електронна пошта" />
+    <input type="password" placeholder="Пароль" />
+    <div class="sub"> Увійти </div>
+    <a href="#page2"> Зареєструватись на {Title} </a>
+    <a href="#page3"> Відновити обліковий запис </a>
+</div>
+</div>
+
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="../js/index.js"></script>
+</body>
+</html>
+    
